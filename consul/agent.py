5cbdee23d421b63f3d674df3ed8d77906b9c163c
Shrivatsa <shrivatsa.swadi@gmail.com>
2021-01-28 20:29:59 +0530

    Added register, deregister and maintenance endpoints

--- consul/agent.py
+++ consul/agent.py
@@ -16,4 +16,13 @@ class Agent:
             self.hosts[f"{host}"] = (res.status_code == 200)
 
     def register(self, service: Service):
-        print("hello")
+        ENDPOINT = "/v1/agent/service/register"
+        httpx.post(ENDPOINT, data=service)
+
+    def deregister(self, service_id: str):
+        ENDPOINT = f"/v1/agent/service/deregister/{service_id}"
+        httpx.post(ENDPOINT)
+
+    def enableMaintenance(self, service_id: str, enable: bool, reason: str):
+        ENDPOINT = f"/v1/agent/service/maintenance/{service_id}?enable={enable}&reason={reason}"
+        httpx.post(ENDPOINT)
